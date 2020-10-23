c15c5975a6197c187af6d54e88ea870eda4c9a0e
malingyan2017 <33071878+malingyan2017@users.noreply.github.com>
2020-10-23 13:47:29 -0700

    modify search file

--- lib/Views/Search/search.dart
+++ lib/Views/Search/search.dart
@@ -1,9 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 import 'package:shopping_app/Views/Search/dropDownButton.dart';
->>>>>>> search_page_lingyan
 
 //https://morioh.com/p/3522d01c11ef
 
@@ -15,20 +12,12 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   String searchName;
 
-<<<<<<< HEAD
-  //variable for the drop down menu
-  int quantity;
-  @override
-  Widget build(BuildContext context) {
-    CollectionReference items = FirebaseFirestore.instance.collection('item');
-=======
   @override
   Widget build(BuildContext context) {
     //items to show when there is no searchName found
     CollectionReference items = FirebaseFirestore.instance.collection('item');
 
     //items to show when there is items matched to searchName
->>>>>>> search_page_lingyan
     Query specificItems = FirebaseFirestore.instance
         .collection('item')
         .where('tags', arrayContains: searchName);
@@ -51,10 +40,6 @@ class _SearchState extends State<Search> {
         Flexible(
           child: StreamBuilder<QuerySnapshot>(
               //if no search, show 3 items, if there is search, show specific items found
-<<<<<<< HEAD
-=======
-
->>>>>>> search_page_lingyan
               stream: (searchName != '' && searchName != null)
                   ? specificItems.snapshots()
                   : items.limit(3).snapshots(),
@@ -65,11 +50,6 @@ class _SearchState extends State<Search> {
                         itemCount: snapshot.data.docs.length,
                         itemBuilder: (context, index) {
                           DocumentSnapshot data = snapshot.data.docs[index];
-<<<<<<< HEAD
-                          //int quantity;
-=======
-
->>>>>>> search_page_lingyan
                           return Card(
                               child: Row(
                             children: [
@@ -77,16 +57,10 @@ class _SearchState extends State<Search> {
                                 width: 60,
                                 height: 60,
                                 decoration: BoxDecoration(
-<<<<<<< HEAD
-                                    image: DecorationImage(
-                                        image:
-                                            NetworkImage(data['pictureUrl']))),
-=======
                                   image: DecorationImage(
                                     image: NetworkImage(data['pictureUrl']),
                                   ),
                                 ),
->>>>>>> search_page_lingyan
                               ),
                               SizedBox(
                                 width: 20,
@@ -94,26 +68,7 @@ class _SearchState extends State<Search> {
                               Column(
                                 children: [
                                   Text('name: ${data['name']}'),
-<<<<<<< HEAD
-                                  DropdownButton<int>(
-                                    hint: Text('quantity'),
-                                    value: quantity,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        quantity = value;
-                                      });
-                                    },
-                                    items: <int>[1, 2, 3, 4, 5, 6, 7]
-                                        .map<DropdownMenuItem<int>>((quantity) {
-                                      return DropdownMenuItem<int>(
-                                        child: Text(quantity.toString()),
-                                        value: quantity,
-                                      );
-                                    }).toList(),
-                                  ),
-=======
                                   MyDropDownButton(),
->>>>>>> search_page_lingyan
                                   RaisedButton(
                                       child: Text('add to cart'),
                                       onPressed: () {}),
