4a0004000f3065e93e4a8810c98c7329ed238f4e
malingyan2017 <33071878+malingyan2017@users.noreply.github.com>
2020-11-20 09:53:58 -0800

    add loading

--- lib/Views/Authentication/register.dart
+++ lib/Views/Authentication/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/Database/auth_state.dart';
+import 'package:shopping_app/Components/centered_loading_circle.dart';
 
 //some of the idea of the form was borrowed from below tutorial's url
 //https://github.com/iamshaunjp/flutter-firebase/blob/lesson-10/brew_crew/lib/screens/home/home.dart
@@ -20,90 +21,101 @@ class _RegisterState extends State<Register> {
   String email = '';
   String password = '';
   String error = '';
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-        actions: [
-          FlatButton.icon(
-            //if pressed, it will show sign in screen
-            onPressed: () {
-              widget.changeViews();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-          )
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage('https://uago.at/-o0SC/sgim.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(children: <Widget>[
-            SizedBox(
-              height: 30,
+    return loading
+        ? CenteredLoadingCircle()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Register'),
+              actions: [
+                FlatButton.icon(
+                  //if pressed, it will show sign in screen
+                  onPressed: () {
+                    widget.changeViews();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                )
+              ],
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'E-mail',
-                border: OutlineInputBorder(),
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://uago.at/-o0SC/sgim.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
-              validator: (value) =>
-                  value.isEmpty ? 'Please enter an email' : null,
-              onChanged: (val) {
-                setState(() {
-                  email = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Password(at least 6 characters)',
-                border: OutlineInputBorder(),
+              child: Form(
+                key: _formKey,
+                child: Column(children: <Widget>[
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'E-mail',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) =>
+                        value.isEmpty ? 'Please enter an email' : null,
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Password(At Least 6 Characters)',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) =>
+                        value.length < 6 ? 'Enter At Least 6 Characters' : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  RaisedButton(
+                    child: Text('Register'),
+                    textColor: Colors.black,
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        setState(() {
+                          loading = true;
+                        });
+                        dynamic result = await _auth.registerWithEmailPassword(
+                            email, password);
+                        if (result == null) {
+                          setState(() {
+                            error =
+                                'Register Failed: Please Provide a Valid Email';
+                            loading = false;
+                          });
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    child: Text(
+                      error,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  )
+                ]),
               ),
-              validator: (value) =>
-                  value.length < 6 ? 'enter at lease 6 characters' : null,
-              obscureText: true,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
             ),
-            SizedBox(
-              height: 30,
-            ),
-            RaisedButton(
-              child: Text('Register'),
-              textColor: Colors.black,
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  dynamic result =
-                      await _auth.registerWithEmailPassword(email, password);
-                  if (result == null) {
-                    setState(() {
-                      error = 'register failed';
-                    });
-                  }
-                }
-              },
-            ),
-            SizedBox(
-              child: Text(error),
-            )
-          ]),
-        ),
-      ),
-    );
+          );
   }
 }
