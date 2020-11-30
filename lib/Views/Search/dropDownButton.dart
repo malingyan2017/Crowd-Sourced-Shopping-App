43e6a6cd8bca7b9054a52d358de6acbadcc519d7
malingyan2017 <33071878+malingyan2017@users.noreply.github.com>
2020-11-30 10:31:52 -0800

    fix overflow on pix 3a

--- lib/Views/Search/dropDownButton.dart
+++ lib/Views/Search/dropDownButton.dart
@@ -22,58 +22,60 @@ class _MyDropDownButtonState extends State<MyDropDownButton> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<TheUser>(context);
-    return Row(
-      children: [
-        DropdownButton<int>(
-          hint: Text('quantity'),
-          value: quantity,
-          onChanged: (value) {
-            setState(() {
-              quantity = value;
-            });
-          },
-          items: <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-              .map<DropdownMenuItem<int>>((quantity) {
-            return DropdownMenuItem<int>(
-              child: Text(quantity.toString()),
-              value: quantity,
-            );
-          }).toList(),
-        ),
-        SizedBox(width: 130),
-        //Positioned(
-        // left: 50,
-        //child:
-        RaisedButton(
-          //color: Colors.blue[200],
-          child: Text('Add to List'),
-          textColor: Colors.black,
-          onPressed: () async {
-            var result = await DatabaseService(uid: user.uid)
-                .validateQuantity(widget.data['barcode'], quantity);
-            if (result == true) {
-              await DatabaseService(uid: user.uid).addToCart(
-                widget.docId,
-                widget.data['name'],
-                widget.data['pictureUrl'],
-                quantity,
-                widget.data['barcode'],
+    return Expanded(
+      child: Row(
+        children: [
+          DropdownButton<int>(
+            hint: Text('quantity'),
+            value: quantity,
+            onChanged: (value) {
+              setState(() {
+                quantity = value;
+              });
+            },
+            items: <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+                .map<DropdownMenuItem<int>>((quantity) {
+              return DropdownMenuItem<int>(
+                child: Text(quantity.toString()),
+                value: quantity,
               );
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text('Item(s) Has Been Added.'),
-              ));
-            } else {
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text(
-                  'Maxium Quantity is 10 for Each Item',
-                  style: TextStyle(color: Colors.red),
-                ),
-              ));
-            }
-          },
-        ),
-        //),
-      ],
+            }).toList(),
+          ),
+          SizedBox(width: 100),
+          //Positioned(
+          // left: 50,
+          //child:
+          RaisedButton(
+            //color: Colors.blue[200],
+            child: Text('Add to List'),
+            textColor: Colors.black,
+            onPressed: () async {
+              var result = await DatabaseService(uid: user.uid)
+                  .validateQuantity(widget.data['barcode'], quantity);
+              if (result == true) {
+                await DatabaseService(uid: user.uid).addToCart(
+                  widget.docId,
+                  widget.data['name'],
+                  widget.data['pictureUrl'],
+                  quantity,
+                  widget.data['barcode'],
+                );
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text('Item(s) Has Been Added.'),
+                ));
+              } else {
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    'Maxium Quantity is 10 for Each Item',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ));
+              }
+            },
+          ),
+          //),
+        ],
+      ),
     );
   }
 }
